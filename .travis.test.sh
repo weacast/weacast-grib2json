783c7ba7dbf916f2bad57382d70cef5f187a07d6
Luc Claustres <luc.claustres@orange.fr>
2020-12-14 21:40:26 +0100

    Try to fix travis test [skip build]

--- .travis.test.sh
+++ .travis.test.sh
@@ -5,5 +5,5 @@ then
 else
 	source .travis.env.sh
 	docker build -f Dockerfile.test -t weacast/weacast-grib2json:test .
-	docker run --rm -v ./coverage:/tmp/weacast-grib2json/coverage weacast/weacast-grib2json:test
+	docker run --rm -v $PWD/coverage:/tmp/weacast-grib2json/coverage weacast/weacast-grib2json:test
 fi
